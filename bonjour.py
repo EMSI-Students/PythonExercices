2efc173c1da866717f4070905e6ac8d5fe77cb75
karkouri-zakaria <92269313+karkouri-zakaria@users.noreply.github.com>
2022-03-30 18:45:15 +0000

    Add files via upload

---
+++ bonjour.py
@@ -0,0 +1,18 @@
+"""Module qui affiche bonjour"""
+
+VAR = "Bonjour"
+
+
+def bonjour(nom):
+    """Dit Bonjour."""
+    return "Bonjour " + nom
+
+
+def ciao(nom):
+    """Dit Ciao."""
+    return "Ciao " + nom
+
+
+def hello(nom):
+    """Dit Hello."""
+    return "Hello " + nom
