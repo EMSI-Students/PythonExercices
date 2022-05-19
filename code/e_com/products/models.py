621f3ef757d98d226b8d133537a25075d257cbc1
Hamza Jamal <hamzajamal.phd@gmail.com>
2022-05-19 18:55:34 +0100

    Add Models for the Second Django Project

--- code/e_com/products/models.py
+++ code/e_com/products/models.py
@@ -1,3 +1,6 @@
 from django.db import models
 
-# Create your models here.
+class Product(models.Model):
+    name = models.CharField(max_length=25)
+    brand = models.CharField(max_length=25)
+    year = models.IntegerField()
--- code/e_com/products/views.py
+++ code/e_com/products/views.py
@@ -1,15 +1,7 @@
 from django.shortcuts import render
-from .product import Product
+from .models import Product
 
-products = []
-products.append(Product('Macbook Air', 'Apple', 2021))
-products.append(Product('Macbook Air', 'Apple', 2020))
-products.append(Product('Macbook Pro', 'Apple', 2021))
-products.append(Product('Macbook Pro', 'Apple', 2020))
-products.append(Product('Thinkpad X', 'Lenovo', 2018))
-products.append(Product('EliteBook', 'Hp', 2018))
-products.append(Product('Airpods Pro', 'Apple', 2020))
-
+products = Product.objects.all()
 
 def products_list(request):
     return render(request, "products/products_list.html", {'list_products': products})
