a16820460a1781ea5c2bf496f48054ab33392900
Hamza Jamal <hamzajamal.phd@gmail.com>
2022-05-28 18:23:13 +0100

    Filter Products using Model's Filter Method

--- code/e_com/products/views.py
+++ code/e_com/products/views.py
@@ -1,18 +1,19 @@
 from django.shortcuts import render
 from .models import Product
 
-products = Product.objects.all()
-
 def products_list(request):
+    products = Product.objects.all()
     return render(request, "products/products_list.html", {'list_products': products})
 
-
 def products_year(request, year):
-    return render(request, "products/products_list_year.html", {'list_products': products, 'year': year})
+    products = Product.objects.filter(year=year)
+    return render(request, "products/products_list.html", {'list_products': products})
 
 def products_name(request, name):
     full_name = " ".join(name.split('_'))
-    return render(request, "products/products_list_name.html", {'list_products': products, 'name': full_name})
+    products = Product.objects.filter(name=full_name)
+    return render(request, "products/products_list.html", {'list_products': products})
 
 def products_brand(request, brand):
-    return render(request, "products/products_list_brand.html", {'list_products': products, 'brand': brand})
+    products = Product.objects.filter(brand=brand)
+    return render(request, "products/products_list.html", {'list_products': products})
